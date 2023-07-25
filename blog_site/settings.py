21cb3fd364244d0efb59ca9a1cbc554f7d1c9686
dadfar <dadfar.momeni@gmail.com>
2023-07-25 13:18:39 +0330

    static url & media url edited

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS=["https://aminollahi.com", "https://buyeragentfinder.com.au"]
 # Application definition
 
 INSTALLED_APPS = [
